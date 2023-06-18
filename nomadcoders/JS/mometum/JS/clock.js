f45febead174a95a6894e84d119c5c5e7ba1cee7
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-18 23:30:45 +0900

    auto interval clock

--- nomadcoders/JS/mometum/JS/clock.js
+++ nomadcoders/JS/mometum/JS/clock.js
@@ -2,8 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
 	const date = new Date();
-	console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+	const hours = String(date.getHours()).padStart(2, "0");
+	const minutes = String(date.getMinutes()).padStart(2, "0");
+	const seconds = String(date.getSeconds()).padStart(2, "0");
+
+	clock.innerText = `${hours}:${minutes}:${seconds}`;
+	// console.log(clock.innerText);
 }
 
-setTimeout(getClock, 5000);
-setInterval(getClock, 5000);
+getClock();
+setInterval(getClock, 1000);
