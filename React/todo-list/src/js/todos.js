f7ef91a2b628cdd63112d466ac21871f591fa8ef
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-24 23:13:34 +0900

    Todo-List
    todo-list 제작
    components 분리

---
+++ React/todo-list/src/js/todos.js
@@ -0,0 +1,6 @@
+export const todoLists = [
+  { id: 1, title: "React 공부하기(1)", content: "React 강의 듣고 정리하기", set: false },
+  { id: 2, title: "React 공부하기(2)", content: "React 강의 듣고 정리하기", set: false },
+  { id: 3, title: "React 공부하기(3)", content: "React 강의 듣고 정리하기", set: false },
+  { id: 4, title: "React 공부하기(4)", content: "React 강의 듣고 정리하기", set: false },
+]
