48081e0a7b0d9dc2402f91d08d483c165cf46a58
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-07-03 09:27:00 +0900

    style component

---
+++ React/styled/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+            <span>Default Style 테스트</span>
+            <h1>제목입니다.</h1>
+			<p>p태그입니다</p>
+		</div>
+	);
+}
+
+export default App;
