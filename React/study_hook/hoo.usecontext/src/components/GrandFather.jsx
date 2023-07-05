bb75c957356ad12b549559487c6fc95837d6101f
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-07-06 01:42:39 +0900

    Update GrandFather.jsx

--- React/study_hook/hoo.usecontext/src/components/GrandFather.jsx
+++ React/study_hook/hoo.usecontext/src/components/GrandFather.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Father from "./Father";
+import { FamilyContext } from "../context/FamilyContext";
 
 function GrandFather() {
 	const houseName = "스파르타";
 	const pocketMoney = 10000;
 
 	return (
-		<Father houseName={houseName} pocketMoney={pocketMoney}>
-			GrandFather
-		</Father>
+		<FamilyContext.Provider
+			value={{
+				houseName,
+				pocketMoney,
+			}}>
+			<Father />
+		</FamilyContext.Provider>
 	);
 }
 
