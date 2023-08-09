50db3e66f9a66f660b871b192c1d63342004ecca
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-08-09 23:41:40 +0900

    chart

---
+++ chartJS/chartjs01/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import Chart01 from "./Components/Chart01";
+import Chart02 from "./Components/Chart02";
+import Chart03 from "./Components/Chart03";
+import Chart04 from "./Components/Chart04";
+import Chart05 from "./Components/Chart05";
+// import Chart06 from "./Components/Chart06";
+// import Chart07 from "./Components/Chart07";
+
+function App() {
+	return (
+		<div className='App'>
+			{/* <Chart01 /> */}
+			<Chart02 />
+			<Chart03 />
+			<Chart04 />
+			<Chart05 />
+			{/* <Chart06 /> */}
+			{/* <Chart07 /> */}
+		</div>
+	);
+}
+
+export default App;
